fef87589b477f3622925276865a9dd24946e6ca5
JusticeSwann <90804100+JusticeSwann@users.noreply.github.com>
2024-10-24 19:55:24 -0400

    developing getting_started page --incomplete

--- lib/presentation/splash/pages/splash.dart
+++ lib/presentation/splash/pages/splash.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spotify_clone/core/configs/assets/app_vectors.dart';
+import 'package:spotify_clone/presentation/intro/pages/get_started.dart';
 
 class SpalshPage extends StatefulWidget {
   const SpalshPage({super.key});
@@ -13,6 +14,7 @@ class _SpalshPageState extends State<SpalshPage> {
   @override
   void initState() {
     super.initState();
+    redirect();
   }
 
   @override
@@ -23,6 +25,14 @@ class _SpalshPageState extends State<SpalshPage> {
           AppVectors.logo
         )
       ),
+    );
+  }
+
+  Future<void> redirect() async{
+    await Future.delayed(const Duration(seconds: 2));
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (BuildContext context) => const GetStartedPage())
     );
   }
 }
